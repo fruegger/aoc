b39d3d2a7912ccf80b00394e8efd36a247966db7
FjR <f.ruegger@flashcable.ch>
2024-12-14 15:22:08 +0100

    dasy 12, 13 and 14

--- 2024/day12/day12.go
+++ 2024/day12/day12.go
@@ -8,110 +8,145 @@ import (
 
 func main() {
 
-	lines := common.StartDay(12, "test")
+	lines := common.StartDay(12, "input")
 
-	regions := findRegions(lines)
-
+	regions := floodFill(lines)
+	total := 0
 	for _, r := range regions {
-		fmt.Println("Type: ", string(r.plantType), " Plots: ", r.plots)
-	}
+		total += len(r.plots) * r.edges
 
+	}
+	fmt.Println("Part1: ", total)
+
+	total = 0
+	marks := makeMarks(lines)
+	for _, r := range regions {
+		sides := r.countSides(marks)
+		total += len(r.plots) * sides
+	}
+	fmt.Println("Part2: ", total)
+}
+
+type Plot struct {
+	p     pos.Position
+	edgeL bool
+	edgeR bool
+	edgeT bool
+	edgeB bool
 }
 
 type Region struct {
-	plantType          uint8
-	plots              []pos.Position
-	markedForMergeInto *Region
+	plantType uint8
+	plots     []Plot
+	edges     int
 }
 
-func (r *Region) belongs(plot pos.Position, t uint8) bool {
-	if r.plantType != t {
-		return false
-	}
-	for _, p := range r.plots {
-		if plotsAreNeighbours(p, plot) {
-			return true
+func (r Region) find(x int, y int) *Plot {
+	for i := 0; i < len(r.plots); i++ {
+		if r.plots[i].p.X == x && r.plots[i].p.Y == y {
+			return &r.plots[i]
 		}
 	}
-	return false
+	return nil
 }
 
-func plotsAreNeighbours(plot1 pos.Position, plot2 pos.Position) bool {
-	dist := plot1.DistanceTo(plot2)
-	if dist.Dx == 0 && (dist.Dy == 1 || dist.Dy == -1) {
-		return true
+func (r Region) countSides(marks [][]bool) int {
+	result := r.edges
+	for _, p1 := range r.plots {
+		for _, p2 := range r.plots {
+			if !(marks[p1.p.Y][p1.p.X] || marks[p2.p.Y][p2.p.X]) {
+				if (p1.p.X-p2.p.X == 1 || p1.p.X-p2.p.X == -1) && p1.p.Y == p2.p.Y {
+					if p1.edgeB && p2.edgeB {
+						result--
+					}
+					if p1.edgeT && p2.edgeT {
+						result--
+					}
+				}
+				if (p1.p.Y-p2.p.Y == 1 || p1.p.Y-p2.p.Y == -1) && p1.p.X == p2.p.X {
+					if p1.edgeL && p2.edgeL {
+						result--
+					}
+					if p1.edgeR && p2.edgeR {
+						result--
+					}
+				}
+			}
+		}
+		marks[p1.p.Y][p1.p.X] = true
 	}
-	if dist.Dy == 0 && (dist.Dx == 1 || dist.Dx == -1) {
-		return true
-	}
-	return false
+	return result
 }
-
-func findRegions(lines []string) []Region {
+func floodFill(lines []string) []Region {
 	var result []Region
-	for y := 0; y < len(lines); y++ {
-		for x := 0; x < len(lines[y]); x++ {
-			p := pos.Position{X: x, Y: y}
-			plantType := lines[y][x]
-			found := false
 
-			// find a region neighboring this plot
-			var region *Region
-			for i := 0; i < len(result) && !found; i++ {
-				found = result[i].belongs(p, plantType)
-				if found {
-					region = &result[i]
+	marks := makeMarks(lines)
+	for y := 0; y < len(lines); y++ {
+		for x := 0; x < len((lines)[y]); x++ {
+			if !marks[y][x] {
+				newRegion := Region{
+					plantType: lines[y][x],
+					plots: []Plot{{
+						p:     pos.Position{x, y},
+						edgeL: false,
+						edgeB: false,
+						edgeR: false,
+						edgeT: false,
+					}},
+					edges: 0,
 				}
-			}
-			if found {
-				// if there is a neighbouring region, then add this plot to it
-				region.plots = append(region.plots, p)
-			} else {
-				// otherwise create a new region with this plot in it
-				region = &Region{
-					plantType:          plantType,
-					plots:              []pos.Position{},
-					markedForMergeInto: nil,
-				}
-				region.plots = append(region.plots, p)
-				result = append(result, *region)
+				marks[y][x] = true
+				fillNeighbours(lines, pos.Position{x, y}, &newRegion, &marks)
+				result = append(result, newRegion)
 			}
 		}
 	}
-	return mergeRegions(result)
+	return result
 }
 
-func mergeRegions(r []Region) []Region {
-	for i := 0; i < len(r); i++ {
-		region1 := &(r)[i]
-		for j := 0; j < len(r); j++ {
-			region2 := &(r)[j]
-			if region1 != region2 && region1.plantType == region2.plantType {
-				isNeighbour := false
-				for i := 0; i < len(region1.plots) && !isNeighbour; i++ {
-					for j := 0; j < len(region2.plots) && !isNeighbour; j++ {
-						isNeighbour = plotsAreNeighbours(region1.plots[i], region2.plots[j])
-					}
-				}
-				if isNeighbour && region2.markedForMergeInto == nil {
-					region1.markedForMergeInto = region2
-				}
+func makeMarks(lines []string) [][]bool {
+	marks := make([][]bool, len(lines))
+	for y := 0; y < len(lines); y++ {
+		marks[y] = make([]bool, len((lines)[y]))
+
+		for x := 0; x < len((lines)[y]); x++ {
+			marks[y][x] = false
+		}
+	}
+	return marks
+}
+
+func fillNeighbours(lines []string, p pos.Position, newRegion *Region, marks *[][]bool) {
+	fillAndMark(lines, p, pos.UP, newRegion, marks)
+	fillAndMark(lines, p, pos.RIGHT, newRegion, marks)
+	fillAndMark(lines, p, pos.DOWN, newRegion, marks)
+	fillAndMark(lines, p, pos.LEFT, newRegion, marks)
+}
+
+func fillAndMark(lines []string, p pos.Position, d pos.Direction, newRegion *Region, marks *[][]bool) {
+	p2 := p.Move(d)
+	if p2.Y >= 0 && p2.Y < len(lines) && p2.X >= 0 && p2.X < len(lines[p2.Y]) &&
+		!(*marks)[p2.Y][p2.X] && newRegion.plantType == lines[p2.Y][p2.X] {
+		newRegion.plots = append(newRegion.plots, Plot{p2, false, false, false, false})
+		(*marks)[p2.Y][p2.X] = true
+		fillNeighbours(lines, p2, newRegion, marks)
+	} else {
+		if p2.Y < 0 || p2.Y >= len(lines) || p2.X < 0 || p2.X >= len(lines[p2.Y]) ||
+			newRegion.plantType != lines[p2.Y][p2.X] {
+			newRegion.edges++
+			plot := newRegion.find(p.X, p.Y)
+			if d == pos.UP {
+				plot.edgeT = true
+			}
+			if d == pos.DOWN {
+				plot.edgeB = true
+			}
+			if d == pos.LEFT {
+				plot.edgeL = true
+			}
+			if d == pos.RIGHT {
+				plot.edgeR = true
 			}
 		}
 	}
-	var result2 []Region
-	for i := 0; i < len(r); i++ {
-		region1 := &r[i]
-		if region1.markedForMergeInto == nil {
-			for _, region2 := range r {
-				if region2.markedForMergeInto == region1 {
-					for j := 0; j < len(region2.plots); j++ {
-						region1.plots = append(region1.plots, region2.plots[j])
-					}
-				}
-			}
-			result2 = append(result2, *region1)
-		}
-	}
-	return result2
 }
