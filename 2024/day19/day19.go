dc176691eb3eb0f7568a31f6e58f26ae5dd7b349
FjR <f.ruegger@flashcable.ch>
2024-12-22 15:36:10 +0100

    day 19; bug fixed

--- 2024/day19/day19.go
+++ 2024/day19/day19.go
@@ -9,63 +9,70 @@ import (
 var towels []string
 
 func main() {
-	lines := common.StartDay(19, "test")
+	lines := common.StartDay(19, "input")
 	towels = strings.Split(lines[0], ", ")
 	var patterns []string
 	for i := 2; i < len(lines); i++ {
 		patterns = append(patterns, lines[i])
 	}
 
-	var ok bool
-	total := 0
+	var result [][]int
+	total1 := 0
+	total2 := 0
 	for _, p := range patterns {
-		var result []int
-		fmt.Println(p, "---")
-		result, ok = sequence(p+"*", 0, result)
-		if ok {
-			total++
+		fmt.Println(p)
+		var part []int
+		result = sequence(p+"*", 0, &part)
+		//		fmt.Println(result)
+		if len(result) > 0 {
+			total1++
 		}
+		total2 += len(result)
 	}
 
-	fmt.Println("Part1 : ", total)
+	fmt.Println("Part1 : ", total1)
+	fmt.Println("Part2 : ", total2)
 }
 
-func sequence(pattern string, pos int, accepted []int) ([]int, bool) {
+func sequence(pattern string, pos int, accepted *[]int) [][]int {
 	matches := resetMatchers()
 	matchersLeft := len(matches)
-
+	var result [][]int
 	for i := 0; i+pos < len(pattern) && (matchersLeft > 0); i++ {
 		for j := 0; j < len(towels) && (matchersLeft > 0); j++ {
-			towel := towels[j]
+			towel := towels[j] + "*"
+			var accepted2 []int
 			if matches[j] {
 				if i < len(towel) {
-					if towel[i] != pattern[i+pos] {
-						matches[j] = false
-						matchersLeft--
-					}
-				} else {
-					wasaccept := accepted
-					accepted = append(accepted, j)
-					if pattern[pos+i] != '*' {
-						accepted, matches[j] = sequence(pattern, pos+i, accepted)
-						if !matches[j] && len(accepted) > 0 {
-							accepted = wasaccept
-						} else {
-							if pattern[pos+i] != '*' {
-								return wasaccept, true
-							}
+					if towel[i] != '*' {
+						if towel[i] != pattern[i+pos] {
+							matches[j] = false
+							matchersLeft--
 						}
 					} else {
-						return accepted, true
+						accepted2 = append(*accepted, j)
+						if pattern[pos+i] != '*' {
+							result2 := sequence(pattern, pos+i, &accepted2)
+							for _, v := range result2 {
+								result = append(result, v)
+							}
+							*accepted = accepted2
+						} else {
+							*accepted = []int{}
+							result = append(result, accepted2)
+							//matches[j] = false
+							//matchersLeft--
+							//							return result, true
+						}
 					}
-					//todo check?
+				} else {
 					matches[j] = false
 					matchersLeft--
 				}
 			}
 		}
 	}
-	return accepted, matchersLeft > 0
+	return result
 }
 
 func resetMatchers() []bool {
