5aba750bd0553ed3387167cc8a796893d048576a
FjR <f.ruegger@flashcable.ch>
2024-12-27 20:48:22 +0100

    day 19

--- 2024/day19/day19.go
+++ 2024/day19/day19.go
@@ -11,67 +11,64 @@ var towels []string
 func main() {
 	lines := common.StartDay(19, "input")
 	towels = strings.Split(lines[0], ", ")
+
 	var patterns []string
 	for i := 2; i < len(lines); i++ {
 		patterns = append(patterns, lines[i])
 	}
 
-	var result [][]int
+	var result int
 	total1 := 0
 	total2 := 0
 	for _, p := range patterns {
 		fmt.Println(p)
-		var part []int
-		result = sequence(p+"*", 0, &part)
-		//		fmt.Println(result)
-		if len(result) > 0 {
+		result = sequence(p + "*")
+		if result > 0 {
 			total1++
 		}
-		total2 += len(result)
+		total2 += result
 	}
 
 	fmt.Println("Part1 : ", total1)
 	fmt.Println("Part2 : ", total2)
 }
 
-func sequence(pattern string, pos int, accepted *[]int) [][]int {
+var cache = map[string]int{}
+
+func sequence(pattern string) int {
+	res, found := cache[pattern]
+	if found {
+		return res
+	}
 	matches := resetMatchers()
 	matchersLeft := len(matches)
-	var result [][]int
-	for i := 0; i+pos < len(pattern) && (matchersLeft > 0); i++ {
+	var result int
+	for i := 0; i < len(pattern) && (matchersLeft > 0); i++ {
 		for j := 0; j < len(towels) && (matchersLeft > 0); j++ {
-			towel := towels[j] + "*"
-			var accepted2 []int
+			towel := towels[j] + "#"
 			if matches[j] {
-				if i < len(towel) {
-					if towel[i] != '*' {
-						if towel[i] != pattern[i+pos] {
-							matches[j] = false
-							matchersLeft--
-						}
-					} else {
-						accepted2 = append(*accepted, j)
-						if pattern[pos+i] != '*' {
-							result2 := sequence(pattern, pos+i, &accepted2)
-							for _, v := range result2 {
-								result = append(result, v)
-							}
-							*accepted = accepted2
-						} else {
-							*accepted = []int{}
-							result = append(result, accepted2)
-							//matches[j] = false
-							//matchersLeft--
-							//							return result, true
-						}
+				if towel[i] != '#' {
+					if towel[i] != pattern[i] {
+						matches[j] = false
+						matchersLeft--
 					}
 				} else {
-					matches[j] = false
-					matchersLeft--
+					if pattern[i] != '*' {
+						result2 := sequence(pattern[i:])
+						result += result2
+						matches[j] = false
+						matchersLeft--
+
+					} else {
+						result++
+						cache[pattern] = result
+						return result
+					}
 				}
 			}
 		}
 	}
+	cache[pattern] = result
 	return result
 }
 
